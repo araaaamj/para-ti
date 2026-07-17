836659ce3456acc929e19f83091ee52ed042c7ff
araaaamj <amorenojauregui05@gmail.com>
2026-07-17 14:57:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// JavaScript
+const stars = document.getElementById("stars");
+
+for(let i=0;i<180;i++){
+
+    const star=document.createElement("div");
+
+    star.classList.add("star");
+
+    const size=Math.random()*3+1;
+
+    star.style.width=size+"px";
+    star.style.height=size+"px";
+
+    star.style.left=Math.random()*100+"vw";
+    star.style.top=Math.random()*100+"vh";
+
+    star.style.animationDuration=(Math.random()*4+2)+"s";
+
+    stars.appendChild(star);
+
+}
+
+document.getElementById("enterButton").addEventListener("click",()=>{
+
+    alert("Aquí aparecerá la siguiente parte de la historia ❤️");
+
+});
